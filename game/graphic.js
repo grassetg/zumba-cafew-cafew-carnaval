eb8eb732dcbbb53f8263ecf93cf47b0c85e0ab4e
Guillaume Grasset <guillaume.grasset@epita.fr>
2020-09-30 20:26:32 +0200

    ennemy: is displayed

--- game/graphic.js
+++ game/graphic.js
@@ -30,6 +30,9 @@ function init()
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(startPosition[0], startPosition[1]), 0);
     scene.add(player1.graphic);
 
+    player2 = new Player("enemy", 0xff0000, new THREE.Vector2(50, 0), 0);
+    scene.add(player2.graphic)
+
     light1 = new Light("sun", 0xffffff, "0,0,340");
     scene.add(light1);
 }
